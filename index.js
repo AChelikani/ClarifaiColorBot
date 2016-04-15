066fa4096a46d9f522e7eab5c7849de78a177ca0
Advith Chelikani <advith.chelikani@gmail.com>
2016-04-14 17:46:38 -0700

    test run name

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var curl = require('node-curl')
 var request = require('request')
 var app = express()
 
@@ -23,6 +24,22 @@ app.get('/webhook/', function (req, res) {
     }
     res.send('Error, wrong token')
 })
+
+function getNameFromID(id) {
+    var request = new XMLHttpRequest();
+    var data = "";
+    request.open('GET', 'https://graph.facebook.com/v2.6/' + id + '?fields=first_name&access_token=EAAOB3vqlxuYBAJTO8BPFuWaWQxRPyFvFUS7ZCdUigBeSugbax6z0U2cJ7fPMmwixnvnPv84iyPOoCzZB25oSZAf6zIuwpMa7gCwZBggbAKhW11IZAj483vapXrPvLKZAKI5HiB02VTZBCCjQtaZBRRuiAoL1D0Hg0JMjMM97SR5aZAQZDZD', false);
+
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            data = JSON.parse(request.responseText);
+        }
+    };
+
+    request.send();
+
+    return data.first_name;
+};
 
 function sendTextMessage(sender, text) {
     messageData = {
@@ -53,7 +70,7 @@ app.post('/webhook/', function (req, res) {
         sender = event.sender.id
         if (event.message && event.message.text) {
             text = event.message.text
-            sendTextMessage(sender, "Hello " + sender)
+            sendTextMessage(sender, "Hello " + getNameFromID(sender))
         }
     }
     res.sendStatus(200)
